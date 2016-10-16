2040beedc2288e85328a7e276c76f49e9d940bf6
Rachel Munoz <rachelumunoz@gmail.com>
2016-10-16 11:55:54 -0700

    Add links to navbar to add gallery and view gallery

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :galleries, only: [:new, :create]
+  resources :galleries, only: [:new, :create, :index]
   root 'galleries#index'
   resources :users, only: [:show, :edit, :update]
 end
