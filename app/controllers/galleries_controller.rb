c0bc5fba09e76d5a06840ca10a1e1a01cb0375e3
Rachel Munoz <rachelumunoz@gmail.com>
2016-10-15 20:02:46 -0700

    Adds add_gallery user tests

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,5 +1,20 @@
-class GalleriesController < ApplicationController
+ class GalleriesController < ApplicationController
   def index
+    @galleries = Gallery.all
   end
 
+  def new
+    @gallery = Gallery.new
+  end
+
+  def create
+    @gallery = Gallery.new(gallery_params)
+    @gallery.save
+    redirect_to root_path
+  end
+
+  private
+    def gallery_params
+      params.require(:gallery).permit(:name, :address)
+    end
 end
--- spec/features/user_creates_gallery_spec.rb
+++ spec/features/user_creates_gallery_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 RSpec.describe "Create Gallery" do 
   scenario "Successfully" do 
     visit root_path
-    click_on "Submit Gallery"
+    click_on "Add Gallery"
+    
     fill_in "Name", with: "The Gallery"
     fill_in "Address", with: "123 Gallery Street"
-    click_on "Submit"
+    click_on "Create Gallery"
 
     expect(page).to have_content "The Gallery"
   end
