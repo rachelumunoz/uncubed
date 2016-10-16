299f2368444c55531cb111eb59c219078a62dd10
Rachel Munoz <rachelumunoz@gmail.com>
2016-10-16 10:33:31 -0700

    Add before_actions to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
 class UsersController < ApplicationController
-  
+  before_action: :authenticate_user!, only: [:edit, :update]
+  before_action: :check_authorization, only: [:edit, :updat
+  before_action :set_user
+
+  def show
+  end
+
+  private
+    def check_authorization
+      unless current_user.id === params[:id]
+        redirect_to root_url
+      end
+    end
+
+    def set_user
+      @user = current_user
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :galleries, only: [:new, :create]
   root 'galleries#index'
+  resources :users, only: [:show]
 end
