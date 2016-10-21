ca8e2fe5451c7bc3cab03a3fa580ba985c6c5f34
Rachel Munoz <rachelumunoz@gmail.com>
2016-10-20 17:15:54 -0700

    Attempts to reactify bootstrap navbar

--- app/assets/javascripts/components/Main.js.jsx
+++ app/assets/javascripts/components/Main.js.jsx
@@ -3,6 +3,7 @@ var Main = React.createClass({
   render: function (){
     return (
       <div>
+       <Nav/>
        <Galleries galleries={this.props.galleries} />
       </div>
     )
---
+++ app/assets/javascripts/components/Nav.js.jsx
@@ -0,0 +1,15 @@
+var Nav = React.createClass({
+  render: function(){
+    return (
+      <div>
+       <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">       
+            <a src="/"> Home </a>
+          </div>
+        </div>
+       </nav>
+      </div>
+    )
+  }
+})
