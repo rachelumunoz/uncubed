8dd73c061e4671fe79a19be6dff18dc2edb87c1a
Rachel Munoz <rachelumunoz@gmail.com>
2016-11-06 19:20:34 -0800

    Adds display of images to gallery show

--- app/assets/javascripts/galleries.js
+++ app/assets/javascripts/galleries.js
@@ -10,6 +10,7 @@
     }).done(function(res){
       $('#new-content').html(res)
     }).error(function(err){
+      //do error things
       console.log('error', err)
     })
   })
@@ -18,15 +19,16 @@
     e.preventDefault()
 
     var url = $(this).attr('href')
+    
     $.ajax({url: url, dataType: 'text'})
     .done(function(res){
-      console.log($('#new-review-form').length)
       if($('#new-review-form').length === 0){
         var form = $(res).insertAfter('#page-heading')
       }
       
       $('#new-review-form').toggle()
     }).error(function(err){
+      //do error things
       console.log('error', err)
     })
   })
