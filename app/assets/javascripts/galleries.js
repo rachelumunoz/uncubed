34bd2ffae790a4c1f65ff00410f528cc84a808bd
Rachel Munoz <rachelumunoz@gmail.com>
2016-11-22 17:18:25 -0800

    Syles review section of gallery show

--- app/assets/javascripts/galleries.js
+++ app/assets/javascripts/galleries.js
@@ -15,29 +15,28 @@
     })
     $('#new-content').toggle()
   })
-
-
       
-  $('body').on('submit', '#galleries-form', function(e){
-    e.preventDefault()
-    console.log('form submitted')
-  })
-  // $('#add-review').on('click', function(e){
+  // $('body').on('submit', '#galleries-form', function(e){
   //   e.preventDefault()
-
-  //   var url = $(this).attr('href')
-    
-  //   $.ajax({url: url, dataType: 'text'})
-  //   .done(function(res){
-  //     if($('#new-review-form').length === 0){
-  //       var form = $(res).insertAfter('#page-heading')
-  //     }
-      
-  //     $('#new-review-form').toggle()
-  //   }).error(function(err){
-  //     //do error things
-  //     console.log('error', err)
-  //   })
+  //   console.log('form submitted')
   // })
+  
+  $('body').on('click', '#add-review', function(e){
+    e.preventDefault()
+
+    $.ajax({
+      method: 'get',
+      url: 'comments/new',
+      dataType: 'text'
+    }).done(function(res){
+      $('#new-content').html(res)
+    }).error(function(err){
+      //do error things
+      console.log('error', err)
+    })
+    $('#new-content').toggle()
+
+  })
+
 
 })
