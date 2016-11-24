18ff40f3e1c9642f0c6d528e3749a5b985ceb811
Rachel Munoz <rachelumunoz@gmail.com>
2016-11-23 19:00:11 -0800

    Adds jquery to show add image form

--- app/assets/javascripts/galleries.js
+++ app/assets/javascripts/galleries.js
@@ -1,5 +1,6 @@
  $( document ).on('turbolinks:load', function() {
   
+  //form for adding a gallery
   $('body').on('click', '#add-gallery', function(e){
     e.preventDefault()
     var that = $(this)
@@ -15,27 +16,18 @@
     })
     $('#new-content').toggle()
   })
-      
-  // $('body').on('submit', '#galleries-form', function(e){
-  //   e.preventDefault()
-  //   console.log('form submitted')
-  // })
-  
-  $('body').on('click', '#add-review', function(e){
+   
+
+  //form for adding images    
+  $('body').on('click', '#add-image', function(e){
     e.preventDefault()
+    var imageForm = $('.add-image')
 
-    $.ajax({
-      method: 'get',
-      url: 'comments/new',
-      dataType: 'text'
-    }).done(function(res){
-      $('#new-content').html(res)
-    }).error(function(err){
-      //do error things
-      console.log('error', err)
-    })
-    $('#new-content').toggle()
-
+    if (imageForm.hasClass('hidden')){
+      $('.add-image').removeClass('hidden')
+    }else{
+      $('.add-image').addClass('hidden')
+    }
   })
 
 
